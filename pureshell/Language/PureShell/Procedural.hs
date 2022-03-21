5a34c24167a198b997b2951f32b1d1575c829cdf
Björn Gohla <b.gohla@gmx.de>
2022-03-21 01:12:46 +0000

    DONE implement record (object) manipulation

--- pureshell/Language/PureShell/Procedural.hs
+++ pureshell/Language/PureShell/Procedural.hs
@@ -16,23 +16,45 @@ import qualified Language.Bash               as Bash (Annotated (..),
                                                       VarName (..), literal)
 
 import qualified Language.Bash.Syntax        as Bash (Identifier (..),
-                                                      SpecialVar (..))
+                                                      SpecialVar (..),
+                                                      Trim (..))
 import qualified Language.Bash.Test          as Bash (Test (..), test)
 
 newtype FunName = FunName { getFunName :: ByteString } deriving newtype (Show, Eq, Ord)
 
 newtype VarName = VarName { getVarName :: ByteString } deriving newtype (Show, Eq, Ord)
 
+newtype ObjectName = ObjectName { getObjectName :: ByteString } deriving newtype (Show, Eq, Ord)
+
+newtype FieldName = FieldName { getFieldName :: ByteString } deriving newtype (Show, Eq, Ord)
+
 data Module l = Module [FunDef l] deriving (Show, Eq, Ord)
 
 data FunClosure = ClosureFromName FunName
                 | ClosureFromVar VarName deriving (Show, Eq, Ord)
 
-data Statement l = Literal l
-                 | Application FunClosure [VarName]
-  deriving (Show, Eq, Ord)
+data ObjectCommand = EmptyObject ObjectName
+                   | DecodeObject ObjectName VarName
+                   | EncodeObject VarName ObjectName
+                   | UpdateField ObjectName FieldName VarName
+                   | ProjectField VarName ObjectName FieldName
+                 deriving (Show, Eq, Ord)
 
-data Assignment l = Assignment VarName (Statement l) deriving (Show, Eq, Ord)
+data Statement l = Literal l -- TODO we should probably rename this to 'Expression'
+                 | Application FunClosure [VarName]
+                 deriving (Show, Eq, Ord)
+
+data Assignment l = Assignment VarName (Statement l)
+                  -- TODO this could be generalized to allow a
+                  -- sequence of assignments to precede the statement
+                  | ObjectCommand ObjectCommand
+                  -- TODO add a constructor for case blocks. QUESTION:
+                  -- what do we match on?
+                  -- | Case VarName VarName [CaseBranch]
+                  deriving (Show, Eq, Ord)
+
+-- TODO define a data type for a sequence of assignments, ending in a
+-- "statement". we can share this between case branches and fundefs
 
 data FunDef l = FunDef FunName [VarName] [Assignment l] (Statement l) deriving (Show, Eq, Ord)
 
@@ -55,6 +77,43 @@ instance (ToBashExpression l) => ToBashStatement (Statement l) where
                ClosureFromName n -> Bash.Literal $ Escape.bash $ getFunName n
                ClosureFromVar v  -> readFromVar $ getVarName v
         readFromVar = Bash.ReadVarSafe . Bash.VarIdent . Bash.Identifier
+
+instance ToBashStatement (ObjectCommand) where
+  toBashStatement = \case
+    EmptyObject o     -> Bash.Declare $ Bash.Dict (Bash.Identifier $ getObjectName o) []
+    DecodeObject o v  -> Bash.SimpleCommand d [a, v']
+      where
+        -- We have to build this command using literals
+        -- unfortunately. 'Assignment.Dict' is no flexible enough.
+        d  = Bash.Literal $ Escape.bash "declare"
+        a  = Bash.Literal $ Escape.bash "-A"
+        v' = Bash.UnescapedLiteral $ (getObjectName o) <> "=\"${" <> getVarName v <> "}\""
+    EncodeObject v o  -> appendBashStatements $ fmap (Bash.Assign . v') [e, (Bash.Trim Bash.ShortestLeading v'' p)]
+      where
+        v'  = Bash.Var w
+        v'' = Bash.VarIdent w
+        w   = Bash.Identifier $ getVarName v
+        o'  = Bash.Literal $ Escape.bash $ getObjectName o
+        e   = Bash.Eval $ Bash.Annotated () $ Bash.SimpleCommand d [Bash.Literal $ Escape.bash "-p", o']
+        d   = Bash.Literal $ Escape.bash "declare"
+        p   = Bash.Literal $ Escape.bash $ "*="
+    UpdateField o f v -> Bash.Assign $ Bash.Var field update
+      where
+        field  = Bash.Identifier $ getObjectName o <> "[" <> getFieldName f <> "]" -- TODO this should use DictUpdate
+        update = Bash.ReadVar $ Bash.VarIdent $ Bash.Identifier $ getVarName v
+    ProjectField v o f -> Bash.Assign $ v' (Bash.ReadArray o' f')
+      where
+        v' = Bash.Var $ Bash.Identifier $ getVarName v
+        o' = Bash.Identifier $ getObjectName o
+        f' = Bash.Literal $ Escape.bash $ getFieldName f
+
+instance (ToBashExpression l) => ToBashStatement (Assignment l) where
+  toBashStatement = \case
+    Assignment v s -> Bash.Local $ Bash.Var i e
+      where
+        i = Bash.Identifier $ getVarName v
+        e = Bash.Eval $ Bash.Annotated () $ toBashStatement s
+    ObjectCommand o -> toBashStatement o
 
 appendBashStatements :: [Bash.Statement ()] -> Bash.Statement ()
 appendBashStatements [] = Bash.Empty
@@ -83,18 +142,13 @@ instance (ToBashExpression l) => ToBashStatement (FunDef l)  where
           posbind (i, v) = Bash.Local $ Bash.Var (bindvar v) $ Bash.ReadVar $ posvar i
           bindvar = Bash.Identifier . getVarName
           posvar = Bash.VarSpecial . Bash.DollarNat
-      as'= fmap assignment as
-        where
-          assignment (Assignment v s) = Bash.Local $ Bash.Var i e
-            where
-              i = Bash.Identifier $ getVarName v
-              e = Bash.Eval $ Bash.Annotated () $ toBashStatement s
+      as'= fmap toBashStatement as
 
 instance (ToBashExpression l) => ToBashStatement (Module l) where
   toBashStatement (Module fs) = go fs
     where
       go [] = Bash.Empty
-      go (f:fs) = Bash.Sequence a b
+      go (g:gs) = Bash.Sequence a b
         where
-          a = Bash.Annotated () $ toBashStatement f
-          b = Bash.Annotated () $ go fs
+          a = Bash.Annotated () $ toBashStatement g
+          b = Bash.Annotated () $ go gs

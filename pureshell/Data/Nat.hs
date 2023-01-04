604c79d8a4c9b6273cc06bf67519f58d8d948443
Björn Gohla <b.gohla@gmx.de>
2023-01-04 18:30:49 +0000

    WIP some fin set arithmetic. enumerate permutations

--- pureshell/Data/Nat.hs
+++ pureshell/Data/Nat.hs
@@ -1,19 +1,33 @@
 {-# LANGUAGE DataKinds                #-}
+{-# LANGUAGE ExplicitNamespaces       #-}
 {-# LANGUAGE GADTs                    #-}
+{-# LANGUAGE InstanceSigs             #-}
 {-# LANGUAGE KindSignatures           #-}
+{-# LANGUAGE LambdaCase               #-}
 {-# LANGUAGE QuasiQuotes              #-}
 {-# LANGUAGE ScopedTypeVariables      #-}
+{-# LANGUAGE StandaloneDeriving       #-}
 {-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE TemplateHaskell          #-}
 {-# LANGUAGE TypeApplications         #-}
 {-# LANGUAGE TypeFamilies             #-}
+{-# LANGUAGE TypeOperators            #-}
 {-# LANGUAGE UndecidableInstances     #-}
-{-# LANGUAGE TypeOperators #-}
 module Data.Nat where
 
 import           Data.Singletons.Base.TH
+import           GHC.TypeLits            (Natural, type (-))
+-- import qualified GHC.TypeLits.Singletons as TS (Natural)
 
-data Nat = Z | S Nat deriving (Show, Ord, Eq)
+data Nat = Z | S Nat deriving (Ord, Eq)
+
+-- | facilitate entering type level nats
+-- ghci> :kind!  N 3
+-- N 3 :: Nat
+-- = 'S ('S ('S 'Z))
+type family N (n :: Natural) :: Nat where
+  N 0 = 'Z
+  N n = 'S (N (n - 1))
 
 genSingletons [''Nat]
 
@@ -21,6 +35,23 @@ $(singletons [d|
                add :: Nat -> Nat -> Nat
                add Z n     = n
                add (S m) n = S (add m n)
+
+               mult :: Nat -> Nat -> Nat
+               mult Z _     = Z
+               mult (S m) n = n `add` (mult m n)
+
+               fact :: Nat -> Nat
+               fact Z     = S Z
+               fact (S n) = (S n) `mult` (fact n)
+
+               deriving instance (Show Nat)
+
+               -- literal patterns are not supported yet by the singletons library
+               --
+               -- nN :: TS.Natural -> Nat
+               -- nN = \case
+               --   0 -> Z
+               --   n -> nN (n - 1)
                |]
  )
 
@@ -34,6 +65,30 @@ lemSuccAddRight SZ _n = Refl
 lemSuccAddRight (SS m) n = case lemSuccAddRight m n of
                              Refl -> Refl
 
+factMult :: Sing ('S m) -> Sing (Fact m) -> Sing (Fact ('S m))
+factMult sm fm = sm `sMult` fm
+
+factFact :: Sing (Fact ('S m)) -> (Sing ('S m), Sing (Fact m))
+factFact = undefined
+
 natToInteger :: Nat -> Integer
-natToInteger Z = 0
+natToInteger Z     = 0
 natToInteger (S n) = 1 + natToInteger n
+
+lemZeroMult :: Sing n -> Sing ('Z `Mult` n) :~: Sing 'Z
+lemZeroMult SZ     = Refl
+lemZeroMult (SS n) = lemZeroMult n
+
+instance Num Nat where
+  (+) = add
+  (*) = mult
+  (-) = error "minus is not implemented for Nat"
+  negate = error "negate is  not implemented for Nat"
+  abs = id
+  signum = const 1
+  fromInteger = \case
+    0 -> Z
+    n -> S (fromInteger $ n - 1)
+
+natFact :: Sing (('S m) `Mult` n) -> Sing ('S m) -> Sing n
+natFact k (SS m) = undefined -- ???
---
+++ pureshell/Data/Permutation/Enumerate.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE GADTs      #-}
+{-# LANGUAGE LambdaCase #-}
+module Data.Permutation.Enumerate where
+
+import           Data.Fin         (Fin (..))
+import           Data.Nat         (Fact, Nat (..), SNat (..))
+import           Data.Permutation (Perm (..))
+
+import           Data.Singletons  (Sing)
+
+perm :: Sing n -> Fin (Fact n) -> Perm n
+perm n (FinZ _)      = Ident n
+perm (SS n) (FinS m) = _
